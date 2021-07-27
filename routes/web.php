acd6873828e524939eef194d2a09c202c7bfb71c
kaichi <satokai0124@gmail.com>
2021-07-27 18:38:52 +0900

    tasksのroutingとviewのheadまで。

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return redirect('/tasks');});
+
+Route::get('/tasks', 'TaskController@index');
+Route::post('/tasks', 'TaskController@store');
+Route::delete('/tasks/(task)', 'TaskController@delete');
+
+\URL::forceScheme('https');
